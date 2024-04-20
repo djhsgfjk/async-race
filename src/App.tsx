8c8e06ce0f13993fedba715ab55c2e81b3497f00
Diana Butiakova <butyakova01@mail.ru>
2024-04-20 13:18:22 +0600

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Async Race
+      </header>
+    </div>
+  );
+}
+
+export default App;
